c2663027579d1d73077f6547cc8e8aab8a496de1
Nada Elshaarawy <nadaelsharaawy103@gmail.com>
2020-05-15 16:39:39 +0200

    upload database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-east.06.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_c39172629f7facf'),
+            'username' => env('DB_USERNAME', 'be8e72bfa73966'),
+            'password' => env('DB_PASSWORD', '79365aab '),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
